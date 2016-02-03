16c4dc0e39841a2cc337b7de7ee8d31797cf98fc
collinbentley1 <collin.bentley@me.com>
2016-02-03 17:53:40 +0000

    Added splash

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+var exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-  res.send('HI!');
+    res.render('index');
 });
+
 
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on port' + process.env.PORT);
