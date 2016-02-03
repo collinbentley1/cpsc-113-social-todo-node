6f98921fa79d544e6794fa52a34f06487d9004a1
collinbentley1 <collin.bentley@me.com>
2016-02-03 19:51:27 +0000

    Added mongoDB

--- index.js
+++ index.js
@@ -3,18 +3,41 @@
 var express = require('express');
 var app = express();
 var exphbs  = require('express-handlebars');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var Users = require('./models/users.js');
 
 //Configure//
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+//Home//
+
 app.get('/', function (req, res) {
-    res.render('index');
+   Users.count(function (err, users) {
+       if (err) {
+           res.send ('error getting users');
+           
+       }else{
+       res.render('index', {userCount : users.length});
+   }
+   });
 });
+
+
 app.post('/user/register', function (req, res) {
-    res.send(req.body);
+    var newUser = new Users();
+    newUser.hashed_password = req.body.password;
+    newUser.email = req.body.email;
+    newUser.name = req.body.fl_name;
+    newUser.save(function(err){
+        if (err){
+            res.send('Unfortunately, we are not accepting new users at this time.')
+        }else{
+            res.redirect('/');
+        }
+        })
+    
     console.log('The user has the email address' , req.body.email);
 });
 app.post('/user/login', function (req, res) {
