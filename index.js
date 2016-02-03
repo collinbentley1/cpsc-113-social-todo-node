d93dd3d02158e60294d5de6be2c49b5585956d0c
collinbentley1 <collin.bentley@me.com>
2016-02-03 18:54:33 +0000

    Parse forms that have been submitted

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+//Require//
+
 var express = require('express');
 var app = express();
 var exphbs  = require('express-handlebars');
+var bodyParser = require('body-parser')
 
+//Configure//
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
+    res.render('index');
+});
+app.post('/user/register', function (req, res) {
+    res.send(req.body);
+    console.log('The user has the email address' , req.body.email);
+});
+app.post('/user/login', function (req, res) {
     res.render('index');
 });
 
