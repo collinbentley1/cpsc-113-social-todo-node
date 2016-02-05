13d0dab9d67c004cf1a26ed540e622915efc0821
collinbentley1 <collin.bentley@me.com>
2016-02-05 03:57:45 +0000

    Add tasks and collaborate. Login issue when one user logs out and another tries to log in.

--- index.js
+++ index.js
@@ -3,11 +3,12 @@
 var express = require('express');
 var app = express();
 var exphbs  = require('express-handlebars');
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var Users = require('./models/users.js');
+var Tasks = require('./models/tasks.js');
 var session = require('express-session');
 var MongoDBStore = require('connect-mongodb-session')(session);
-
 //Database for storing sessionIds on the server//
 
 var store = new MongoDBStore({ 
@@ -17,6 +18,7 @@ var store = new MongoDBStore({
 
 
 //Configure//
+mongoose.connect(process.env.MONGO_URL);
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -26,7 +28,7 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: 'auto' },
   store: store
-}))
+}));
 
 app.use(function(req, res, next){
     if(req.session.userId){
@@ -35,29 +37,48 @@ app.use(function(req, res, next){
                res.locals.currentUser = user;
            }
            next();
-       })
+       });
     }else{
         next();
     }
-})
+});
+
+//Define middleware to ensure that there is a user logged in before posting to form//
+
+function isLoggedIn(req, res, next){
+    
+   if(res.locals.currentUser){
+       next();
+       
+   }else{
+    res.sendStatus(403);
+}
+}
+
+//load user tasks//
+function loadUserTasks(req, res, next) {
+    if (!res.locals.currentUser){
+        return next();
+    }
+    Tasks.find({}).or([
+    {owner: res.locals.currentUser},
+    {collaborators: res.locals.currentUser.email}])
+    .exec(function(err, tasks){
+        if(!err){
+            res.locals.tasks = tasks;
+        }
+        next();
+    });
+}
+
 
 //Above passes to one of the below//
 
-app.get('/', function (req, res) {
-   Users.count(function (err, users) {
-       if (err) {
-           res.send ('error getting users');
-           
-       }else{
-       res.render('index', {
-           userCount : users.length,
-           currentUser: res.locals.currentUser
-
-
-       });
-   }
-   });
+app.get('/', loadUserTasks, function (req, res) {
+      res.render('index');
+      
 });
+
 
 
 app.post('/user/register', function (req, res) {
@@ -83,13 +104,13 @@ app.post('/user/register', function (req, res) {
 app.get('/user/logout', function(req, res){
     req.session.destroy();
     res.redirect('/');
-})
+});
 
 
 app.post('/user/login', function (req, res) {
     var user = Users.findOne({email: req.body.email}, function(err, user){
         if(err || !user){
-            res.send('Incorrect email or password.')
+            res.send;
             return;
         }
         user.comparePassword(req.body.password, function (err, isMatch){
@@ -98,15 +119,34 @@ app.post('/user/login', function (req, res) {
 
         }else{
         req.session.userId = user._id;
-        res.redirect('/')  
+        res.redirect('/')  ;
         }
-    })
-    })
+    });
+    });
 });
 
 
+app.use(isLoggedIn);
+
+app.post('/tasks/create', function (req, res){
+    var newTask = new Tasks();
+    newTask.owner = res.locals.currentUser._id;
+    newTask.title= req.body.title;
+    newTask.description=req.body.description;
+    newTask.collaborators=[req.body.collaborator1, req.body.collaborator2, req.body.collaborator3];
+    newTask.save(function(err, savedTask){
+        if (err || !savedTask){
+            res.send('Error saving task!');
+            
+        }else{
+            res.redirect('/');
+        
+        }
+    });
+
+});
+
+// initialize the server
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on port' + process.env.PORT);
 });
-
-
