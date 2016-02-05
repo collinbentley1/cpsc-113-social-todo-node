acd4b80dbfe31fe539b1a34787197b016aa70d12
collinbentley1 <collin.bentley@me.com>
2016-02-05 04:43:49 +0000

    scrapped code and copied and pasted

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-//Require// //Must Initialize MONGO_URL and SESSION_SECRET//
-
 var express = require('express');
-var app = express();
 var exphbs  = require('express-handlebars');
-var mongoose = require('mongoose');
+var app = express();
 var bodyParser = require('body-parser');
-var Users = require('./models/users.js');
-var Tasks = require('./models/tasks.js');
 var session = require('express-session');
 var MongoDBStore = require('connect-mongodb-session')(session);
-//Database for storing sessionIds on the server//
-
-var store = new MongoDBStore({ 
-    uri: process.env.MONGO_URL,
-    collection: 'sessions'
-      });
-
-
-//Configure//
+var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL);
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+var Users = require('./models/users.js');
+var Tasks = require('./models/tasks.js');
+
+
+
+// Configure our app
+var store = new MongoDBStore({
+  uri: process.env.MONGO_URL,
+  collection: 'sessions'
+});
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+// Configure session middleware that will parse the cookies
+// of an incoming request to see if there is a session for this cookie.
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
@@ -30,67 +29,64 @@ app.use(session({
   store: store
 }));
 
+// Middleware that looks up the current user for this sesssion, if there
+// is one.
 app.use(function(req, res, next){
-    if(req.session.userId){
-       Users.findById(req.session.userId, function(err, user){
-           if(!err){
-               res.locals.currentUser = user;
-           }
-           next();
-       });
-    }else{
-        next();
-    }
+  if(req.session.userId){
+    Users.findById(req.session.userId, function(err, user){
+      if(!err){
+        res.locals.currentUser = user;
+      }
+      next();
+    });
+  }else{
+    next();
+  }
 });
 
-//Define middleware to ensure that there is a user logged in before posting to form//
-
+// Middleware that checks if a user is logged in. If so, the
+// request continues to be processed, otherwise a 403 is returned.
 function isLoggedIn(req, res, next){
-    
-   if(res.locals.currentUser){
-       next();
-       
-   }else{
+  if(res.locals.currentUser){
+    next();
+  }else{
     res.sendStatus(403);
-}
+  }
 }
 
-//load user tasks//
+// Middleware that loads a users tasks if they are logged in.
 function loadUserTasks(req, res, next) {
-    if (!res.locals.currentUser){
-        return next();
-    }
-    Tasks.find({}).or([
-    {owner: res.locals.currentUser},
-    {collaborators: res.locals.currentUser.email}])
+  if(!res.locals.currentUser){
+    return next();
+  }
+  Tasks.find({}).or([
+      {owner: res.locals.currentUser},
+      {collaborators: res.locals.currentUser.email}])
     .exec(function(err, tasks){
-        if(!err){
-            res.locals.tasks = tasks;
-        }
-        next();
-    });
+      if(!err){
+        res.locals.tasks = tasks;
+      }
+      next();
+  });
 }
 
-
-//Above passes to one of the below//
-
+// Return the home page after loading tasks for users, or not.
 app.get('/', loadUserTasks, function (req, res) {
       res.render('index');
-      
 });
 
-
-
+// Handle submitted form for new users
 app.post('/user/register', function (req, res) {
-    if (req.body.password !== req.body.password_confirmation){
-        return res.render('index', {errors: "Your password and password confimation do not match."});
-    }
-      if (req.body.password.length < 1) {
+  if(req.body.password !== req.body.password_confirmation){
+      return res.render('index', {errors: "Password and password confirmation do not match"});
+  }
+  if (req.body.password.length < 1) {
     err = 'Bad password';
     res.render('index', {errors: err});
     return;
   }
- // Save the new user
+
+  // Save the new user
   var newUser = new Users();
   newUser.hashed_password = req.body.password;
   newUser.email = req.body.email;
@@ -111,10 +107,6 @@ app.post('/user/register', function (req, res) {
   });
 });
 
-app.get('/user/logout', function(req, res){
-    req.session.destroy();
-    res.redirect('/');
-});
 
 
 app.post('/user/login', function (req, res) {
@@ -138,27 +130,34 @@ app.post('/user/login', function (req, res) {
   });
 });
 
-app.use(isLoggedIn);
-
-app.post('/tasks/create', function (req, res){
-    var newTask = new Tasks();
-    newTask.owner = res.locals.currentUser._id;
-    newTask.title= req.body.title;
-    newTask.description=req.body.description;
-    newTask.collaborators=[req.body.collaborator1, req.body.collaborator2, req.body.collaborator3];
-    newTask.save(function(err, savedTask){
-        if (err || !savedTask){
-            res.send('Error saving task!');
-            
-        }else{
-            res.redirect('/');
-        
-        }
-    });
-
+// Log a user out
+app.get('/user/logout', function(req, res){
+  req.session.destroy(function(){
+    res.redirect('/');
+  });
 });
 
-// initialize the server
+//  All the controllers and routes below this require
+//  the user to be logged in.
+app.use(isLoggedIn);
+
+// Handle submission of new task form
+app.post('/task/create', function(req, res){
+  var newTask = new Tasks();
+  newTask.owner = res.locals.currentUser._id;
+  newTask.title = req.body.title;
+  newTask.description = req.body.description;
+  newTask.collaborators = [req.body.collaborator1, req.body.collaborator2, req.body.collaborator3];
+  newTask.save(function(err, savedTask){
+    if(err || !savedTask){
+      res.send('Error saving task!');
+    }else{
+      res.redirect('/');
+    }
+  });
+});
+
+// Start the server
 app.listen(process.env.PORT, function () {
-  console.log('Example app listening on port' + process.env.PORT);
+  console.log('Example app listening on port ' + process.env.PORT);
 });
--- models/users.js
+++ models/users.js
@@ -1,26 +1,27 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
-var SALT_WORK_FACTOR= 10;
+var SALT_WORK_FACTOR = 10;
+
 var Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId;
-    
+
 var stringField = {
-        type: String, 
-        minlength: 1,
-        maxlength: 50
+    type: String,
+    minlength: 1,
+    maxlength: 50
 }
 
 var UserSchema = new Schema({
     email: {
-        type:String,
+        type: String,
         minlength: 1,
         maxlength: 50,
-        lowercase: true
+        lowercase: true,
+        unique: true
     },
     name: stringField,
     hashed_password: stringField
 });
-
 
 UserSchema.pre('save', function(next) {
     var user = this;
@@ -50,8 +51,8 @@ UserSchema.methods.comparePassword = function(candidatePassword, cb) {
     });
 };
 
-// assign a function to the "methods" object of our UserSchema
 UserSchema.statics.count = function (cb) {
   return this.model('Users').find({}, cb);
 }
+
 module.exports = mongoose.model('Users', UserSchema);
